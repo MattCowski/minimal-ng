9862c059f2f23693f83d166308db8b6a637e6077
MattCowski <mattcowski@gmail.com>
2015-03-24 17:48:45 +0000

    livereload watches html and css

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,20 +2,18 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    tinylr = require('tiny-lr')();
 
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
+  app.use(require('connect-livereload')({port: 4002}));
   app.use(express.static(__dirname));
   app.listen(4000);
 });
 
 gulp.task('styles', function() {
-//   return gulp.src('sass/*.scss')
-//     .pipe(sass({ style: 'expanded' }))
-//     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-//     .pipe(gulp.dest('css'))
   return sass('sass/', { style: 'expanded' })
      .pipe(gulp.dest('css'))
     .pipe(rename({suffix: '.min'}))
@@ -25,8 +23,24 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
   gulp.watch('sass/*.scss', ['styles']);
+  gulp.watch('*.html', notifyLiveReload);
+  gulp.watch('css/*.css', notifyLiveReload);
 });
 
-gulp.task('default', ['express', 'watch'], function() {
+gulp.task('livereload', function() {
+  tinylr.listen(4002); //instead of default 35729
+});
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+};
+
+
+gulp.task('default', ['styles', 'express', 'livereload', 'watch'], function() {
 
 });
