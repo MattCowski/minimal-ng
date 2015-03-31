e2db864ded4f36b90daaa80cc15bd93a5c4429a1
MattCowski <mattcowski@gmail.com>
2015-03-31 20:22:08 +0000

    alt port works too

--- Gulpfile.js
+++ Gulpfile.js
@@ -29,9 +29,9 @@ gulp.task('coffee', function() {
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
-  app.use(require('connect-livereload')({port: 4002}));
+  app.use(require('connect-livereload')({port: 3002}));
   app.use(express.static(__dirname+'/public'));
-  app.listen(4000);
+  app.listen(3000);
 });
 
 gulp.task('styles', function() {
@@ -51,7 +51,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('livereload', function() {
-  tinylr.listen(4002); //instead of default 35729
+  tinylr.listen(3002); //instead of default 35729
 });
 function notifyLiveReload(event) {
   var fileName = require('path').relative(__dirname, event.path);
