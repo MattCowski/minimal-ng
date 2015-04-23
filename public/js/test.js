68148c692421b9e58def1b85d50ee37735bac7c3
MattCowski <mattcowski@gmail.com>
2015-04-23 17:19:29 +0000

    simple angular setup

--- public/js/test.js
+++ public/js/test.js
@@ -1,3 +1,8 @@
-var test;
-
-test = "hello world!";
+var app = angular.module("sampleApp", ["firebase"]);
+app.controller("SampleCtrl", function($scope, $firebaseObject) {
+//   var ref = new Firebase("https://<your-firebase>.firebaseio.com/");
+  // download the data into a local object
+//   $scope.data = $firebaseObject(ref);
+  $scope.test = 'testing'
+  // putting a console.log here won't work, see below
+});
